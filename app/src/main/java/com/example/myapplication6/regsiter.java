53c12b2caea1185465137b3f0c8d6841613e511c
amrwa <amrwaleed818@gmail.com>
2022-10-28 01:48:38 +0200

    commit

---
+++ app/src/main/java/com/example/myapplication6/regsiter.java
@@ -0,0 +1,26 @@
+package com.example.myapplication6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class regsiter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regsiter);
+
+        TextView btn=findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(regsiter.this,MainActivity.class));
+
+            }
+        });
+    }
+}
